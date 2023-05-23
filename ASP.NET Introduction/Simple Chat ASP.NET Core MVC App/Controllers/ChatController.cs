df583655e36ca633334456027e79bf93559de60f
Rosen Yordanov <roskata182003@gmail.com>
2023-05-23 16:23:02 +0300

    Upload second part from ASP.NET Core Introduction Exercise

---
+++ ASP.NET Introduction/Simple Chat ASP.NET Core MVC App/Controllers/ChatController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Simple_Chat_ASP.NET_Core_MVC_App.Models;
+
+namespace Simple_Chat_ASP.NET_Core_MVC_App.Controllers
+{
+    public class ChatController : Controller
+    {
+       //For this exercise only
+       private static List<KeyValuePair<string, string>> Messages = new List<KeyValuePair<string, string>>();
+
+        public IActionResult Show()
+        {
+            if (!Messages.Any())
+            {
+                return View(new ChatViewModel());
+            }
+            ChatViewModel chatViewModel = new ChatViewModel()
+            {
+                Messages = Messages.Select((m) => new MessageViewModel()
+                {
+                    Sender = m.Key,
+                    MessageText = m.Value,
+                })
+                .ToList()
+            };
+            return View(chatViewModel);
+        }
+
+        [HttpPost]
+        public IActionResult Send(ChatViewModel chat)
+        {
+            MessageViewModel message = chat.CurrentMessage;
+            Messages.Add(new KeyValuePair<string, string>(message.Sender, message.MessageText));
+            return RedirectToAction("Show");
+        }
+    }
+}
---
+++ ASP.NET Introduction/Simple Chat ASP.NET Core MVC App/Models/MessageViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Simple_Chat_ASP.NET_Core_MVC_App.Models
+{
+    public class MessageViewModel
+    {
+        public string Sender { get; set; } = null!;
+        public string MessageText { get; set; } = null!;
+    }
+}
