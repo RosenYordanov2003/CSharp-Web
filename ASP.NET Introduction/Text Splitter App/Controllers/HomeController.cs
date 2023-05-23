582f7854e30ca4e2c9366d950cbf69c5a4e1b438
Rosen Yordanov <roskata182003@gmail.com>
2023-05-23 17:03:07 +0300

    Upload last part of ASP.NET Exercise

---
+++ ASP.NET Introduction/Text Splitter App/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using Text_Splitter_App.Models;
+
+namespace Text_Splitter_App.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index(TextViewModel textViewModel)
+        {
+            return View(textViewModel);
+        }
+        public IActionResult Split(TextViewModel textViewModel)
+        {
+            string[] splitedText = textViewModel.Text.Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+            textViewModel.SplitText = string.Join(Environment.NewLine, splitedText);
+
+            return RedirectToAction("Index", textViewModel);
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ ASP.NET Introduction/Text Splitter App/Models/TextViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Text_Splitter_App.Models
+{
+    public class TextViewModel
+    {
+        public string Text { get; set; } = null!;
+        public string SplitText { get; set; } = null!;
+    }
+}
